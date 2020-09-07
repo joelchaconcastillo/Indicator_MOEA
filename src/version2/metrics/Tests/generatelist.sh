bb2d2e457010805884134ce9ddbd791fa5f8a502
Joel Chacón Castillo <joel.chacon@cimat.mx>
2020-09-07 17:34:15 -0500

    ..

--- src/version2/metrics/Tests/generatelist.sh
+++ src/version2/metrics/Tests/generatelist.sh
@@ -10,7 +10,6 @@ PATHA=../HV/  #/home/joel.chacon/Current/MyResearchTopics/Data_Decomposition/Sta
        echo ${PATHA}/POF/${i}_2
      done
    done
-  exit; 
    for i in WFG1 WFG2 WFG3 WFG4 WFG5 WFG6 WFG7 WFG8 WFG9 DTLZ1 DTLZ2 DTLZ3 DTLZ4 DTLZ5 DTLZ6 DTLZ7 UF8 UF9 UF10;
    do
      echo "--"${i}_3; 
