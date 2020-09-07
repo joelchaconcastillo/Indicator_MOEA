c467e91d7de36e86821dc51dcf8082ceb90e41a6
chaconcastillojoel <joel.chacon@cimat.mx>
2020-09-07 10:31:40 -0500

    ..

--- src/version2/HomeworkCluster/Generatorexec.pl
+++ src/version2/HomeworkCluster/Generatorexec.pl
@@ -32,7 +32,7 @@ foreach my $Di(@DI)
          {
          	my $nvar;
          	
-         	for(my $nobj = 2; $nobj <=3; $nobj++)
+         	for(my $nobj = 3; $nobj <=3; $nobj++)
          	{
 		  if( $nobj eq 2) { $nWeights =501; }
 		  if( $nobj eq 3) { $nWeights =496; }
@@ -61,7 +61,7 @@ foreach my $Di(@DI)
          {
          	my $nvar;
          	
-         	for(my $nobj = 2; $nobj <=3; $nobj++)
+         	for(my $nobj = 3; $nobj <=3; $nobj++)
          	{
          	   my $k =4;# 2*($nobj-1);
          	   my $l =20;# 24-$k;
@@ -75,18 +75,18 @@ foreach my $Di(@DI)
          	   }
          	}
          }
-	    @Instance = ("UF1", "UF2", "UF3", "UF4", "UF5", "UF6", "UF7");
-	    my $nobj=2;
-	       foreach(@Instance)
-	       {
-	       	   my $nvar=30;
-	       	   for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semilla de cada instancia..
-	       	   {
-		  if( $nobj eq 2) { $nWeights =501; }
-		  if( $nobj eq 3) { $nWeights =496; }
-	       	   	print $fout "~$PathAlgorithm/Ejecutable $PathAlgorithm $_ $Sed $nobj $pops $nWeights $nOffspring $max_nfes $CR $F $nvar $Di $Df\n";
-	       	   }
-	       }
+	 #	    @Instance = ("UF1", "UF2", "UF3", "UF4", "UF5", "UF6", "UF7");
+	 	    my $nobj=2;
+	 #	       foreach(@Instance)
+	 #	       {
+	 #	       	   my $nvar=30;
+	 #	       	   for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semilla de cada instancia..
+	 #	       	   {
+	 #		  if( $nobj eq 2) { $nWeights =501; }
+	 #		  if( $nobj eq 3) { $nWeights =496; }
+	 #	       	   	print $fout "~$PathAlgorithm/Ejecutable $PathAlgorithm $_ $Sed $nobj $pops $nWeights $nOffspring $max_nfes $CR $F $nvar $Di $Df\n";
+	 #	       	   }
+	 #	       }
 	    
 	    @Instance = ("UF8", "UF9", "UF10");
 	    $nobj=3;
